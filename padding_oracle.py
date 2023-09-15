46e5563af8911edfd58c2e02f9909134ed94e928
ekersey <ekersey@umich.edu>
2023-09-15 00:34:15 +0000

    kers first commit

--- padding_oracle.py
+++ padding_oracle.py
@@ -36,7 +36,6 @@ def oracle(url: str, messages: List[bytes]) -> List[Dict[str, str]]:
             time.sleep(10)
             continue
 
-
 def main():
     if len(sys.argv) != 3:
         print(f"usage: {sys.argv[0]} ORACLE_URL CIPHERTEXT_HEX", file=sys.stderr)
@@ -45,10 +44,61 @@ def main():
 
     if oracle(oracle_url, [message])[0]["status"] != "valid":
         print("Message invalid", file=sys.stderr)
+    
+    # Break the message into blocks
 
-    #
-    # TODO: Decrypt the message
-    #
+    numBlocks = int(len(message) / 16)
+    cypherBlocks = []
+
+    for i in range(numBlocks): # Finds the correct ranges to break up the blocks
+
+        j = i * 16
+        k = j + 16
+
+        cypherBlocks.append(message[j:k])
+
+    solvedBlocks = []
+
+    # Should be numBlocks - 1 (Doing this project for every block)
+    for current_block in range(1):
+
+        incoming_block = bytearray(cypherBlocks[current_block])
+        current_block = bytearray(cypherBlocks[current_block + 1])
+        knownBytes = []
+
+        for current_byte in range(4): # Should be 16 (Do this to every byte)
+
+            testReq = []
+
+            for z in range(current_byte): # Part i'm confused with, don't know what to XOR
+
+                incoming_block[15 - z] = knownBytes[z] ^ (current_byte + 2) # Finding the reverse engineered value we want in our edited cyphertext
+
+            for j in range(256): # Creates 256 messages that change only the byte we are focused on
+
+                incoming_block[15 - current_byte] = j # The byte we need to change
+
+                temp = incoming_block + current_block
+
+                testReq.append(bytes(temp))
+
+            retJson = oracle(oracle_url, testReq) # Sends request to oracle
+
+            byteVal = 0 # If output value is negative a invalid_mac was not found
+
+            for j in range(256):
+
+                check = retJson[j]
+
+                if check["status"] == "invalid_mac":
+
+                    byteVal = j
+                    print("Valid")
+
+            knownBytes.append(byteVal ^ current_byte) # Adds decrypted byte to known bytes
+            print(knownBytes)
+
+        solvedBlocks.append(knownBytes)
 
     decrypted = "TODO"
     print(decrypted)
